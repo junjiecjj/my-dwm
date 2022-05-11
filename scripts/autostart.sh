0916f83bb3845baf3885c21eff367faba23f8d94
dell-xps <2716705056@qq.com>
2022-05-11 18:47:02 +0800

    just update

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#笔记本，三个显示器，一个原屏幕，两个外界，下面三行：关闭原屏幕，只用两个外界显示器
+# xrandr --output DP-1-8 --primary
+# xrandr --output DP-1-8 --auto --output eDP-1 --off
+# xrandr --output DP-1-9  --right-of DP-1-8  --auto
+
+
 /bin/bash ./dwm-status.sh &
 /bin/bash ./wp-autochange.sh &
-#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c  --experimental-backends  -b
+picom   --experimental-backends -b
 /bin/bash ./tap-to-click.sh &
 /bin/bash ./inverse-scroll.sh &
 # /bin/bash ~/scripts/setxmodmap-colemak.sh &
