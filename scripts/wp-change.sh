11838f2a6c2faa5e40479d2e25d5a7bdee253fe3
dell-xps <2716705056@qq.com>
2022-05-22 15:28:46 +0800

    just update

--- scripts/wp-change.sh
+++ scripts/wp-change.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-feh --recursive --randomize --bg-fill ~/图片/Wallpapers/
+feh --recursive --randomize --bg-fill   ~/图片/Wallpapers/
+
+# feh --recursive --randomize --bg-fill  $(xdg-user-dir PICTURES)'/Wallpapers/'
+
 #feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
 
 
